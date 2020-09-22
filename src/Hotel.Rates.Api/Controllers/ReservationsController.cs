c077f2dfe93814d4ab0064d60f2a50f479353e44
Carlos Varela <cvarela1496@gmail.com>
2020-09-21 23:15:38 -0600

    adds default template for exam

---
+++ src/Hotel.Rates.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,53 @@
+﻿using Hotel.Rates.Api.Models;
+using Hotel.Rates.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Rates.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly InventoryContext _context;
+
+        public ReservationsController(InventoryContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]ReservationModel reservationModel)
+        {
+            var ratePlan = _context
+                .NightlyRatePlans
+                .Include(r => r.Seasons)
+                .Include(r => r.RatePlanRooms)
+                .ThenInclude(r => r.Room)
+                .First(r => r.Id == reservationModel.RatePlanId);
+            var canReserve = ratePlan.Seasons
+                .Any(s => s.StartDate >= reservationModel.ReservationStart && s.EndDate <= reservationModel.ReservationEnd);
+            var room = ratePlan.RatePlanRooms
+                .First(r => r.RoomId == reservationModel.RoomId && r.RatePlanId == reservationModel.RatePlanId);
+            var isRoomAvailable = room.Room.Amount > 0 &&
+                room.Room.MaxAdults > reservationModel.AmountOfChildren &&
+                room.Room.MaxChildren <= reservationModel.AmountOfChildren;
+
+            if (canReserve && isRoomAvailable)
+            {
+                room.Room.Amount -= 1;
+                _context.SaveChanges();
+                var days = (reservationModel.ReservationEnd - reservationModel.ReservationStart).TotalDays;
+                return Ok(new
+                {
+                    Price = days * ratePlan.Price
+                });
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ src/Hotel.Rates.Api/Models/ReservationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Rates.Api.Models
+{
+    public class ReservationModel
+    {
+        public DateTime ReservationStart { get; set; }
+
+        public DateTime ReservationEnd { get; set; }
+
+        public int RatePlanId { get; set; }
+
+        public int RoomId { get; set; }
+
+        public int AmountOfAdults { get; set; }
+
+        public int AmountOfChildren { get; set; }
+
+    }
+}
